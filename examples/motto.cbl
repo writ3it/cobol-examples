cfec386b0477a1c09963ce4940967116556a3f8b
writ3it <git@writ3it.tech>
2021-02-21 14:58:29 +0100

    motto of the gnucobol

---
+++ examples/motto.cbl
@@ -0,0 +1,17 @@
+      * ----------------------------------------------------------------
+      * The motto of GnuCobol programmers. 
+      * Yes, it's compilable.
+      * ----------------------------------------------------------------
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. motto.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+           1, computer                                               . 2
+           programming value is "Highly rewarding".
+
+
+           2, GnuCOBOL REDEFINES programming                     pic xx.
+                                                               procedure
+                                                               division.
+           3. display GnuCOBOL.
