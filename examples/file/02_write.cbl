4d732696ff47fee3a7ac716b63608f25108b6c41
writ3it <git@writ3it.tech>
2021-02-21 15:32:31 +0100

    file writing

---
+++ examples/file/02_write.cbl
@@ -0,0 +1,59 @@
+      * Rewrite SampleData/persons.txt to build/output.txt
+      * but output.txt is indexed file
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. 01_READ_FILE.
+
+       ENVIRONMENT DIVISION.
+           INPUT-OUTPUT SECTION.
+               FILE-CONTROL.
+               SELECT PERSON-IN ASSIGN TO '../SampleData/persons.txt'
+                   ORGANIZATION IS LINE SEQUENTIAL.
+
+               SELECT PERSON-OUT ASSIGN TO 'output.txt'
+                   ORGANIZATION IS INDEXED
+                   ACCESS IS DYNAMIC
+                   RECORD KEY IS FO_PERSON-SURNAME.
+              
+
+       DATA DIVISION.
+           FILE SECTION.
+           FD PERSON-IN.
+               01 FI_PERSON.
+                  02 FI_PERSON-ID PIC 999.
+                  02 FI_PERSON-NAME PIC X(16).
+                  02 FI_PERSON-SURNAME PIC X(25).
+           
+           FD PERSON-OUT.
+               01 FO_PERSON.
+                  02 FO_PERSON-ID PIC 999.
+                  02 FO_PERSON-NAME PIC X(16).
+                  02 FO_PERSON-SURNAME PIC X(25).
+
+           WORKING-STORAGE SECTION.
+               01 S_PERSON.
+                   02 S_PERSON-ID PIC 999.
+                   02 S_PERSON-NAME PIC X(16).
+                   02 S_PERSON-SURNAME PIC X(25).
+               01 B_EOF PIC A(1) VALUE 'F'.
+
+       PROCEDURE DIVISION.
+           OPEN INPUT PERSON-IN.
+           OPEN OUTPUT PERSON-OUT.
+
+               PERFORM UNTIL B_EOF = 'T'
+                   READ PERSON-IN INTO FO_PERSON
+                       AT END MOVE 'T' TO B_EOF
+                       NOT AT END DISPLAY FO_PERSON
+                   END-READ
+                   WRITE FO_PERSON 
+                   END-WRITE
+               END-PERFORM.
+
+           CLOSE PERSON-OUT.
+           CLOSE PERSON-IN.
+
+
+       STOP RUN.
+
+
+       
